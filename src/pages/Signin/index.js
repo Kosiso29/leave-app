2ffdb551b52c3ff5d208b50e52664255ce05183d
Kosisochukwu Afoenyi <kosisochukwu.afoenyi@platoanalytics.com>
2022-05-27 06:58:15 +0100

    27/5/22 - add microsoft authentication

--- src/pages/Signin/index.js
+++ src/pages/Signin/index.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { Card, Button, Form } from "react-bootstrap";
+import { Card, Button, Form, Spinner } from "react-bootstrap";
 import { connect } from "react-redux";
 import * as actions from "../../store/actions";
 import axios from "../../axios";
@@ -8,41 +8,57 @@ import axios from "../../axios";
 import classes from "./Signin.module.scss";
 
 const Signin = (props) => {
-    const { email, onAuthVerifyEmail } = props;
+    const { onAuthVerifyEmail } = props;
     const { Title, Body } = Card;
     const { Group, Control } = Form;
 
     const navigate = useNavigate();
 
+    const [submitted, setSubmitted] = useState(true);
     const [state, setState] = useState({
         email: '',
-        password: '',
         loggedIn: false,
         error: false
     })
 
-    const handleClick = () => {
+    const handleClick = async () => {
+        setSubmitted(false);
+        
         const data = {
             userEmail: state.email
         }
 
-        axios.post("/UserLogin", data)
+        const microsoftAuthString = await axios.post("/UserLogin", data)
             .then(response => {
+                const output = response.data.data;
                 setState({
                     ...state,
                     loggedIn: true,
                     error: false
                 })
-                console.log("sign in", response);
-            })
-            .then(() => {
-                onAuthVerifyEmail(state.email);
-                navigate('/Dashboard');
+                onAuthVerifyEmail(output.firstName + " " + output.lastName);
+                return output.microsoftAuthString;
             })
             .catch(error => {
+                setSubmitted(true);
                 setState({ ...state, error: true });
                 alert(error);
             })
+        
+        if (microsoftAuthString) {
+            axios.post(microsoftAuthString)
+                .then(() => {
+                    setSubmitted(true);
+                })
+                .then(() => {
+                    navigate('/Dashboard');
+                })
+                .catch(error => {
+                    setSubmitted(true);
+                    setState({ ...state, error: true });
+                    alert(error);
+                })
+        }
     }
 
     const handleChange = (e, input) => {
@@ -64,20 +80,17 @@ const Signin = (props) => {
     return (
         <div className={classes.signin}>
             <Card className={classes.card}>
-                {/* <Img src="https://picsum.photos/200/50" alt="picsum" /> */}
                 <Body>
                     <Title className={classes.title}>Sign In</Title>
                     <Group className={classes.group}>
-                        {/* <Label>Email Address</Label> */}
                         <Control className={classes.control} type="email" placeholder="Your Email" onChange={(e) => { handleChange(e, "email") }} />
-                        {/* <Label>Password</Label> */}
-                        {/* <Control className={classes.control} type="password" placeholder="Password" onChange={(e) => { handleChange(e, "password") }} /> */}
                         <Button
                             className={classes.button}
                             variant="success"
                             onClick={handleClick}
                         >
                             Login
+                        {submitted ? null : <Spinner animation="border" className={classes.spinner} />}
                         </Button>
                     </Group>
                 </Body>
